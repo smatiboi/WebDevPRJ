cb785b976ac0757163f104ddaf083f35639ce2dd
adim chouya <246524953+adimchouyadev-oss@users.noreply.github.com>
2026-05-02 12:31:19 +0100

    Delete backend directory

--- backend/controllers/metaController.js
+++
@@ -1,9 +0,0 @@
-const pool = require('../config/db');
-
-async function getMeta(req, res) {
-  const [departments] = await pool.execute('SELECT id, name, code FROM departments ORDER BY name');
-  const [categories] = await pool.execute('SELECT id, name FROM categories ORDER BY name');
-  res.json({ departments, categories });
-}
-
-module.exports = { getMeta };
--- backend/routes/metaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { asyncHandler } = require('../utils/errors');
-const { getMeta } = require('../controllers/metaController');
-
-const router = express.Router();
-
-router.get('/', asyncHandler(getMeta));
-
-module.exports = router;
